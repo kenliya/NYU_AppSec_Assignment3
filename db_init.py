ee0085681cf91e507c2b86b74de89683c4d77c31
hyc449 <hyc449@nyu.edu>
2019-11-18 00:17:02 -0700

    Updated tables

--- db_init.py
+++ db_init.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref, scoped_session, sessionmaker
 
@@ -19,6 +19,16 @@ class User(Base):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+class Login_History(Base):
+    __tablename__ = 'login_history'
+    username = Column(String(32), unique=True, primary_key=True)
+    session_cookie = Column(String(1000), unique=True)
+    login_timestamp = Column(String(1000))
+    logout_timestamp = Column(String(1000))
+    
+    def __repr__(self):
+        return '<Login_History {}>'.format(self.username)
+        
 # create tables
 #def db_init():
 Base.metadata.create_all(engine)
